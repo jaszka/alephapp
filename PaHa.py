c54faa022c11561bdcee2cab935ad8291e79576f
jaszka <a.jachowicz@o2.pl>
2016-05-13 00:35:28 +0200

    Create PaHa.py
    This is the core

---
+++ PaHa.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import re
+import login
+import sendemail
+
+'''
+Paha to Twoja podręczna ciocia bibliotekarka, która zawsze wie, kiedy masz iść odebrać swoją zamówioną książkę.
+Słuchaj się cioci. A najpierw podaj swój login i hasło do katalogu Aleph, i maila na którego ciocia ma wysyłać całuski
+'''
+
+logon = ''
+haslo = ''
+adres = ''
+
+# Czy zamówienie jest juz dostępne?
+matches = re.findall('Zarezerwowany.+\d', login.loginaleph(logon, haslo))
+
+if len(matches) == 0:
+    sendemail.send(adres, matches)
