8c65d9cb4b1bf5985ab168d01e9167691bad2f14
jaszka <a.jachowicz@o2.pl>
2016-06-25 19:44:39 +0200

    Update PaHa.py

--- PaHa.py
+++ PaHa.py
@@ -1,19 +1,14 @@
 # coding=utf-8
-import re
-import login
-import sendemail
+import functions
 
 '''
 Paha to Twoja podręczna ciocia bibliotekarka, która zawsze wie, kiedy masz iść odebrać swoją zamówioną książkę.
 Słuchaj się cioci. A najpierw podaj swój login i hasło do katalogu Aleph, i maila na którego ciocia ma wysyłać całuski
 '''
 
-logon = ''
-haslo = ''
-adres = ''
+data = functions.read()
 
-# Czy zamówienie jest juz dostępne?
-matches = re.findall('Zarezerwowany.+\d', login.loginaleph(logon, haslo))
+(reservationReady, notification) = functions.checkAleph(data['id'], data['key'])
 
-if len(matches) > 0:
-    sendemail.send(adres, matches)
+if (reservationReady):
+    functions.send(notification, data['to'])
