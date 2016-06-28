d5336eaafb8c2e2060d2348ff4898e43fe654b96
jaszka <a.jachowicz@o2.pl>
2016-06-28 21:08:52 +0200

    Update PaHa.py

--- PaHa.py
+++ PaHa.py
@@ -1,11 +1,10 @@
 # coding=utf-8
-import functions
-
 '''
 Paha to Twoja podręczna ciocia bibliotekarka, która zawsze wie, kiedy masz iść odebrać swoją zamówioną książkę.
 Słuchaj się cioci. A najpierw podaj swój login i hasło do katalogu Aleph, i maila na którego ciocia ma wysyłać całuski
 '''
 
+import functions
 data = functions.read()
 
 (reservationReady, notification) = functions.checkAleph(data['id'], data['key'])
